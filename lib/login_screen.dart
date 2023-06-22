67dff842b431c32d5446332d5a2b3f22f6632e55
itsmedivyhere <114496412+itsmedivyhere@users.noreply.github.com>
2023-06-22 14:56:06 +0530

    Add files via upload

---
+++ lib/login_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => studentState();
+}
+
+class studentState extends State<LoginScreen> {
+  bool college1 = true;
+  bool college2 = true;
+  bool college3 = true;
+
+  void onPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              "LogIn",
+              style: TextStyle(
+                  // fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                  letterSpacing: 2.0,
+                  wordSpacing: 4.0),
+            )),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                color: Colors.blue,
+                child: Icon(
+                  CupertinoIcons.clock_fill,
+                  color: Colors.black,
+                  size: 50,
+                ),
+                // Image.asset(
+                //   'assets/images/login_image.png',
+                //   fit: BoxFit.cover,
+                // ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Icon(
+                      CupertinoIcons.person_alt_circle,
+                      size: 100,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        border: Border.all(),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(16.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        border: Border.all(),
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(16.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle login button press
+                      },
+                      child: Text('Login'),
+                    ),
+                    SizedBox(height: 50),
+                    GestureDetector(
+                      onTap: () {
+                        // Handle sign up link press
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Don\'t remember password? ',
+                          style: TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: 'Click here!',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(LoginScreen());
+}
