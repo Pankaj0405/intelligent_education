f620ce37f9056f6c3e9e84b29e857a881e0c0f11
itsmedivyhere <114496412+itsmedivyhere@users.noreply.github.com>
2023-06-30 11:28:26 +0530

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -15,50 +15,28 @@ class studentState extends State<LoginScreen> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              "LogIn",
-              style: TextStyle(
-                  // fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                  letterSpacing: 2.0,
-                  wordSpacing: 4.0),
-            )),
         body: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
-                // decoration: BoxDecoration(
-                //   image: DecorationImage(
-                //       image:
-                //           AssetImage('intelligent_education/assets/login.png'),
-                //       fit: BoxFit.cover),
-                // ),
-                height: MediaQuery.of(context).size.height * 0.3,
-                color: Colors.blue,
-                child: Icon(
-                  CupertinoIcons.clock_fill,
-                  color: Colors.black,
-                  size: 80,
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: Image.asset(
+                  'images/bgimg.jpeg',
+                  fit: BoxFit.cover,
                 ),
-                // Image.asset(
-                //   'assets/images/login_image.png',
-                //   fit: BoxFit.cover,
-                // ),
               ),
               Padding(
-                padding: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Icon(
                       CupertinoIcons.person_alt_circle,
-                      size: 140,
+                      size: 100,
                       color: Colors.black,
                     ),
-                    SizedBox(height: 20),
+                    SizedBox(height: 10),
                     TextField(
                       style: TextStyle(color: Colors.black),
                       decoration: InputDecoration(
@@ -69,9 +47,7 @@ class studentState extends State<LoginScreen> {
                             borderRadius: BorderRadius.circular(20),
                           )),
                     ),
-                    SizedBox(
-                      height: 30,
-                    ),
+                    SizedBox(height: 20),
                     TextField(
                       style: TextStyle(),
                       obscureText: true,
@@ -83,34 +59,37 @@ class studentState extends State<LoginScreen> {
                             borderRadius: BorderRadius.circular(20),
                           )),
                     ),
-                    SizedBox(height: 40),
+                    SizedBox(height: 20),
                     ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Login'),
+                      onPressed: () {
+                        // Handle details button press
+                        Navigator.pushNamed(context, 'login');
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black),
+                      ),
                       style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.all(20.0),
+                        padding: EdgeInsets.all(10.0),
                         textStyle: TextStyle(fontSize: 20),
                       ),
                     ),
-                    SizedBox(height: 50),
-                    GestureDetector(
-                      onTap: () {},
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Don\'t remember password? ',
-                          style: TextStyle(color: Colors.black),
-                          children: [
-                            TextSpan(
-                              text: 'Click here!',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold,
-                              ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Don\'t remember Password?'),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Click here',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
+                      ],
+                    )
                   ],
                 ),
               ),
@@ -120,8 +99,4 @@ class studentState extends State<LoginScreen> {
       ),
     );
   }
-}
-
-void main() {
-  runApp(LoginScreen());
 }
